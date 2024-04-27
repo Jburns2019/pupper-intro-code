11293c0d9633591ddcc0383d870f2a19db0cc62c
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-04-27 23:28:01 +0000

    print statement

--- StanfordQuadruped-mini_pupper/run_robot.py
+++ StanfordQuadruped-mini_pupper/run_robot.py
@@ -4,8 +4,8 @@ from src.IMU import IMU
 from src.Controller import Controller
 from src.JoystickInterface import JoystickInterface
 from src.State import BehaviorState, State
-from MangDang.mini_pupper.HardwareInterface import HardwareInterface
-from MangDang.mini_pupper.Config import Configuration
+from pupper.HardwareInterface import HardwareInterface
+from pupper.Config import Configuration
 from pupper.Kinematics import four_legs_inverse_kinematics
 from MangDang.mini_pupper.display import Display
 from src.MovementScheme import MovementScheme
