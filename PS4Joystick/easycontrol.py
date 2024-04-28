fe8be4a8db1a1fc3a0074f0dae438966df87b374
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-04-28 00:56:13 +0000

    moves robot

---
+++ PS4Joystick/easycontrol.py
@@ -0,0 +1,17 @@
+from UDPComms import Publisher
+
+a=Publisher(8830,65530)
+a.send({"L1": 1, 
+        "R1": 0, 
+        "x": 0, 
+        "circle": 0, 
+        "triangle": 0, 
+        "L2": 0, 
+        "R2": 0, 
+        "ly": 0, 
+        "lx": 0, 
+        "rx": 0, 
+        "message_rate": 1, 
+        "ry": 0, 
+        "dpady": 0, 
+        "dpadx": 0}) 
