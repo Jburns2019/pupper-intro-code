5107c9bc257a1ca0aa97c8ac961b6d718e002c61
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-04-29 05:55:53 +0000

    update

--- PS4Joystick/easycontrol.py
+++ PS4Joystick/easycontrol.py
@@ -10,4 +10,5 @@ def trot():
 
 # a.send({"L1": 1, "R1": 0, "x": 0, "circle": 0, "triangle": 0, "L2": 0, "R2": 0, "ly": 0, "lx": 0, "rx": 0, "message_rate": 1, "ry": 0, "dpady": 0, "dpadx": 0}) 
 if __name__ == "__main__":
+    activate()
     trot()
