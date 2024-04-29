fa52204c6396db56333c0ddbc1d197fa31b85fc5
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-04-29 05:44:56 +0000

    update.4

--- PS4Joystick/easycontrol.py
+++ PS4Joystick/easycontrol.py
@@ -1,6 +1,6 @@
 from UDPComms import Publisher
 
-
+a=Publisher(8830)
 def activate(a):
     a.send({"L1": 1, 
         "R1": 0, 
@@ -17,22 +17,22 @@ def activate(a):
         "dpady": 0, 
         "dpadx": 0}) 
 
+activate()
+# # a.send({"L1": 1, 
+# #         "R1": 0, 
+# #         "x": 0, 
+# #         "circle": 0, 
+# #         "triangle": 0, 
+# #         "L2": 0, 
+# #         "R2": 0, 
+# #         "ly": 0, 
+# #         "lx": 0, 
+# #         "rx": 0, 
+# #         "message_rate": 1, 
+# #         "ry": 0, 
+# #         "dpady": 0, 
+# #         "dpadx": 0}) 
 
-# a.send({"L1": 1, 
-#         "R1": 0, 
-#         "x": 0, 
-#         "circle": 0, 
-#         "triangle": 0, 
-#         "L2": 0, 
-#         "R2": 0, 
-#         "ly": 0, 
-#         "lx": 0, 
-#         "rx": 0, 
-#         "message_rate": 1, 
-#         "ry": 0, 
-#         "dpady": 0, 
-#         "dpadx": 0}) 
-
-if __name__ == "__main__":
-    a=Publisher(8830)
-    activate(a)
+# if __name__ == "__main__":
+#     a=Publisher(8830)
+#     activate(a)
