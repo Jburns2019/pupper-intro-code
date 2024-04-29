87c6402baf4650dee152a3b35bfc36d9731b3e79
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-04-29 06:09:50 +0000

    update.2

--- PS4Joystick/easycontrol.py
+++ PS4Joystick/easycontrol.py
@@ -1,4 +1,5 @@
 from UDPComms import Publisher
+import time
 # look into joystickinterface.py for control outputs
 
 a=Publisher(8830)
@@ -20,9 +21,40 @@ def activate():
             "dpadx": 0})
 
 def trot():
-    a.send({"R1": 1})
+    a.send({"L1": 1, 
+            "R1": 1, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 1, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+    
+def trot_stop():
+    a.send({"L1": 1, 
+            "R1": 1, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 1, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
 
 # a.send({"L1": 1, "R1": 0, "x": 0, "circle": 0, "triangle": 0, "L2": 0, "R2": 0, "ly": 0, "lx": 0, "rx": 0, "message_rate": 1, "ry": 0, "dpady": 0, "dpadx": 0}) 
 if __name__ == "__main__":
-    activate()
-    # trot()
+    # activate()
+    trot()
+    time.sleep(5)
+    trot_stop()
