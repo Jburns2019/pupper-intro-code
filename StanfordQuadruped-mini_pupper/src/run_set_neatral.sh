f888bb45caea46306dbb3dabc12680af8dc629a2
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-05-01 01:46:31 +0000

    revert

--- StanfordQuadruped-mini_pupper/src/run_set_neatral.sh
+++ StanfordQuadruped-mini_pupper/src/run_set_neatral.sh
@@ -8,14 +8,14 @@ systemctl stop robot
 echo 1500000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
 echo "pwm 0 setting pwm off parameter --> 90 degree "
 
-# echo 1500000 > /sys/class/pwm/pwmchip0/pwm1/duty_cycle
-# echo "pwm 1 setting pwm off parameter --> 90 degree"
+echo 1500000 > /sys/class/pwm/pwmchip0/pwm1/duty_cycle
+echo "pwm 1 setting pwm off parameter --> 90 degree"
 
-# echo 500000 > /sys/class/pwm/pwmchip0/pwm2/duty_cycle
-# echo "pwm 2 setting pwm off parameter --> 0 degree"
+echo 500000 > /sys/class/pwm/pwmchip0/pwm2/duty_cycle
+echo "pwm 2 setting pwm off parameter --> 0 degree"
 
-# echo 2500000 > /sys/class/pwm/pwmchip0/pwm3/duty_cycle
-# echo "pwm 3 setting pwm off parameter --> 180 degree"
+echo 2500000 > /sys/class/pwm/pwmchip0/pwm3/duty_cycle
+echo "pwm 3 setting pwm off parameter --> 180 degree"
 
 sleep 1
 
