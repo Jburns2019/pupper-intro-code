aae43a285ed6b164dce3345f0746548898c4b1ea
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-05-01 07:30:43 +0000

    edting...

--- PS4Joystick/easycontrol.py
+++ PS4Joystick/easycontrol.py
@@ -75,7 +75,7 @@ def stop():
 # a.send({"L1": 1, "R1": 0, "x": 0, "circle": 0, "triangle": 0, "L2": 0, "R2": 0, "ly": 0, "lx": 0, "rx": 0, "message_rate": 1, "ry": 0, "dpady": 0, "dpadx": 0}) 
 if __name__ == "__main__":
     activate()
-    move()
-    time.sleep(5)
-    # trot_stop()
-    stop()
+    # move()
+    # time.sleep(5)
+    # # trot_stop()
+    # stop()
