bded41f91e7fd0642e8a22ab352200d51249397a
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-05-02 05:58:22 +0000

    list

--- StanfordQuadruped-mini_pupper/src/leg_move.py
+++ StanfordQuadruped-mini_pupper/src/leg_move.py
@@ -1,60 +1,16 @@
 #!/usr/bin/env python3
 
-# import numpy as np
-# import time
-# # from src.IMU import IMU
-# # from src.Controller import Controller
-# # from src.JoystickInterface import JoystickInterface
-# from State import State
-# from MangDang.mini_pupper.HardwareInterface import HardwareInterface
-# from pupper.HardwareInterface import HardwareInterface
-# from MangDang.mini_pupper.Config import Configuration
-# from pupper.Kinematics import four_legs_inverse_kinematics
-# from MangDang.mini_pupper.display import Display
-# from src.MovementScheme import MovementScheme
-# from src.danceSample import MovementLib
 import os
 import sys
+import time
 
 def main():
     os.system("sudo systemctl stop robot")
     os.system("echo 1500000 > /sys/class/pwm/pwmchip0/pwm1/duty_cycle")
-    # os.system("echo 0 > /sys/class/gpio/gpio"+ str(servo2_en) + "/value")
-    # state = State()
-    # # config = Configuration()
-    # hardware_interface = HardwareInterface()
-    # # movementCtl = MovementScheme(MovementLib)
-
-    # # if use_imu:
-    # #     imu = IMU(port="/dev/ttyACM0")
-    # #     imu.flush_buffer()
-
-    # # controller = Controller(
-    # #     config,
-    # #     four_legs_inverse_kinematics,
-    # # )
-
-    # # # Read imu data. Orientation will be None if no data was available
-    # quat_orientation = (
-    #     imu.read_orientation() if use_imu else np.array([1, 0, 0, 0])
-    # )
-    # state.quat_orientation = quat_orientation
-
-    # # movementCtl.runMovementScheme()
-    # # legsLocation = movementCtl.getMovemenLegsLocation()
-    # # attitudes = movementCtl.getMovemenAttitude()
-    # # speed = movementCtl.getMovemenSpeed()
-    # # print(speed, attitudes, legsLocation) 
-    # # hardware_interface.set_actuator_postions(state.joint_angles)
-
-    # hardware_interface.set_actuator_position(state.joint_angles,2,0)
+    time.sleep(1)
+    os.system("echo 2500000 > /sys/class/pwm/pwmchip0/pwm1/duty_cycle")
+    time.sleep(1)
+    os.system("echo 500000 > /sys/class/pwm/pwmchip0/pwm1/duty_cycle")
 
 if __name__ == "__main__":
     main()
-
-# leg =  Specifies which leg to control. 0 is front-right, 1 is front-left, 2 is back-right, 3 is back-left.
-# axis = Specifies which joint of leg to control. 0 is abduction servo, 1 is inner hip servo, 2 is outer hip servo.
-# angle = Desired servo angle, relative to the vertical (z) axis
-# pwmparams = PWMParams object
-# def move_legs(pwm_params, servo_params, axis, leg):
-#     [...]
