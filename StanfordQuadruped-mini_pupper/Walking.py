4669bca65ed5d20c37111591d7bc1c0ae6e3149d
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-05-02 09:25:18 +0000

    testing new methods

--- StanfordQuadruped-mini_pupper/Walking.py
+++ StanfordQuadruped-mini_pupper/Walking.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from src.MovementGroup import MovementGroups
+from src.MovementScheme import MovementScheme
 
 def move():
     move = MovementGroups()
@@ -8,6 +9,10 @@ def move():
     move.move_forward()
 
     MovementLib = move.MovementLib
+
+    movementCtl = MovementScheme(MovementLib)
+    
+    movementCtl.runMovementScheme()
 
 if __name__ == "__main__":
     move()
