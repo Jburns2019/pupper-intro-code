891acf70794d7cf735ea39506abbb7dfe96c493d
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-05-03 02:43:27 +0000

    moves?

--- StanfordQuadruped-mini_pupper/src/move.py
+++ StanfordQuadruped-mini_pupper/src/move.py
@@ -1,12 +1,23 @@
-from src.Command import Command
-from src.State import BehaviorState, State
+from Command import Command
+from Controller import Controller
+from State import BehaviorState, State
 import numpy as np
 import time
 from MangDang.mini_pupper.HardwareInterface import HardwareInterface
+from MangDang.mini_pupper.Config import Configuration
+from pupper.Kinematics import four_legs_inverse_kinematics
 
+config = Configuration()
 state = State()
 command = Command()
 hardware_interface = HardwareInterface()
+
+
+# Create controller
+controller = Controller(
+    config,
+    four_legs_inverse_kinematics,
+)
 
 state.quat_orientation = np.array([1,0,0,0])
 
@@ -14,5 +25,5 @@ state.behavior_state = BehaviorState.TROT
 
 command.horizontal_velocity = np.array([0.1,0])
 
-# controller.run(state, command,disp)
+controller.run(state, command)
 hardware_interface.set_actuator_postions(state.joint_angles)
