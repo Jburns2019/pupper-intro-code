c9a2965154e0dad81d0a66375779755338bad753
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-05-03 03:03:49 +0000

    modes

--- PS4Joystick/easycontrol.py
+++ PS4Joystick/easycontrol.py
@@ -1,10 +1,11 @@
 from UDPComms import Publisher
 import time 
 # look into joystickinterface.py for control outputs
-# rover example for how to control more aspects of robot arm_pub = Publisher(8410)
-# drive_pub = Publisher(8830)
-# arm_pub = Publisher(8410)
-a=Publisher(8830)
+# drive_pub = Publisher(8830) = controls movement of pupper (basically mode 1)
+# arm_pub = Publisher(8410) = controls more movements of upper (mode 2)
+# mode 2 is what you can do when pupper is not in trot mode when using a controller.
+drive_pub = Publisher(8830)
+arm_pub = Publisher(8410)
 # L1 = activate/disactivate
 # R1 = transition between Rest mode and Trot mode.
 # circle = dance or hold for 3 seconds to turn off system
@@ -17,7 +18,7 @@ a=Publisher(8830)
 # rx = turn left or right (pitch)
 # ry = pitches the robot forward
 def activate():
-    a.send({"L1": 1, 
+    drive_pub.send({"L1": 1, 
             "R1": 0, 
             "x": 0, 
             "circle": 0, 
@@ -33,7 +34,7 @@ def activate():
             "dpadx": 0})
 
 def trot():
-    a.send({"L1": 0, 
+    drive_pub.send({"L1": 0, 
             "R1": 1, 
             "x": 0, 
             "circle": 0, 
@@ -50,15 +51,15 @@ def trot():
     
     
 def move_forward():
-    a.send({"L1": 0, 
+    drive_pub.send({"L1": 0, 
             "R1": 0, 
             "x": 0, 
             "circle": 0, 
             "triangle": 0, 
             "L2": 0, 
             "R2": 0, 
-            "ly": 0.1, 
-            "lx": 0, 
+            "ly": [0.1, 0], 
+            "lx": [0, 0], 
             "rx": 0, 
             "message_rate": 20, 
             "ry": 0, 
