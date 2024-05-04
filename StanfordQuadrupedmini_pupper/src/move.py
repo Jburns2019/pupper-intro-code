5afd183af267621787718fdc1359483091dd09bf
Rafael Morales Mayoral <76973156+raff17@users.noreply.github.com>
2024-05-04 03:25:04 +0000

    test

--- StanfordQuadrupedmini_pupper/src/move.py
+++ StanfordQuadrupedmini_pupper/src/move.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 from MangDang.mini_pupper.HardwareInterface import HardwareInterface
 from MangDang.mini_pupper.Config import Configuration
-from StanforQuadrupedmini_pupper.src.Kinematics import four_legs_inverse_kinematics
+# from StanforQuadrupedmini_pupper.src.Kinematics import four_legs_inverse_kinematics
 
 config = Configuration()
 state = State()
@@ -13,11 +13,11 @@ command = Command()
 hardware_interface = HardwareInterface()
 
 
-# Create controller
-controller = Controller(
-    config,
-    four_legs_inverse_kinematics,
-)
+# # Create controller
+# controller = Controller(
+#     config,
+#     four_legs_inverse_kinematics,
+# )
 
 state.quat_orientation = np.array([1,0,0,0])
 
@@ -25,5 +25,5 @@ state.behavior_state = BehaviorState.TROT
 
 command.horizontal_velocity = np.array([0.1,0])
 
-controller.run(state, command)
+# controller.run(state, command)
 hardware_interface.set_actuator_postions(state.joint_angles)
