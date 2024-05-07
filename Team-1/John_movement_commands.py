cc98128e07dd1119fe662c1192acf08863572914
John Burns <burnsjo@oregonstate.edu>
2024-05-07 15:46:01 -0700

    Simpler_update.

--- Team-1/John_movement_commands.py
+++ Team-1/John_movement_commands.py
@@ -161,7 +161,7 @@ if __name__ == "__main__":
     wait(1)
     for i in range(500):
         print(f'Run {i + 1}')
-        if i % 100:
+        if i % 100 == 0:
             turn('forward-left')
         else:
             move('forward')
