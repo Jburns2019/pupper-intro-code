8eba090d1ae471c8c8ff4e801878ace60232cf01
John Burns <burnsjo@oregonstate.edu>
2024-05-06 22:24:46 -0700

    Simpler_update.

--- Team-1/John_movement_commands.py
+++ Team-1/John_movement_commands.py
@@ -159,7 +159,7 @@ def send_command(command):
 if __name__ == "__main__":
     activate()
     wait(1)
-    for i in range(100):
+    for i in range(50):
         print(f'Run {i + 1}')
         move('forward')
         wait(1)
