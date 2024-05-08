bb7c8c13605fee4ed9c51907791de6851f9da86f
John Burns <burnsjo@oregonstate.edu>
2024-05-08 14:28:53 -0700

    Simpler_update.

--- Team-1/John_movement_commands.py
+++ Team-1/John_movement_commands.py
@@ -164,7 +164,7 @@ def move_by_feet(amount=3, dir='forward'):
     count = int(amount/3.0*450.0)
     
     for i in range(count):
-        if i % 40 == 0 and dir == 'forward':
+        if i % 60 == 0 and dir == 'forward':
             turn('right')
         else:
             move(dir)
